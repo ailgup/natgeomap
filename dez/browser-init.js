90b9e25c7bbf9f886b418bde720ed6f38ac7d22e
ailgup <ailgup@mail.com>
2021-05-15 10:17:18 -0400

    dez

---
+++ dez/browser-init.js
@@ -0,0 +1,31 @@
+  function downloadURL(dataurl, filename) {
+  var a = document.createElement("a");
+  a.href = dataurl;
+  a.setAttribute("download", filename);
+  a.click();
+  console.log("clicked");
+}
+(function () {
+  document.getElementById("urlform").onsubmit = function (evt) {
+    evt.preventDefault();
+    var url = document.getElementById("url").value;
+    window.location.hash = url;
+	ZoomManager.proxy_tiles="https://ophir.alwaysdata.net/dezoomify/proxy.php";
+    ZoomManager.open(url);
+	
+
+	
+    return false;
+  }
+
+  // allow to set the URL to be dezoomed by setting the URL hash
+  var startURL = window.location.hash.slice(1);
+  if (startURL) {
+    document.getElementById("url").value = startURL;
+  }
+
+  document.querySelector("#popup > button[title=Close]").addEventListener("click", function () {
+    document.getElementById("popup").style.bottom = "-500px";
+  })
+  
+})();
